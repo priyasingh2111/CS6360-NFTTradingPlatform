19171d744e1393de922a7b46b8a372313cdc36ce
t-anya <tanya.sh0610@gmail.com>
2022-11-23 10:17:02 -0600

    refactoring + added date check

--- NFT/main.py
+++ NFT/main.py
@@ -427,6 +427,7 @@ def daterange_transaction_history():
         print(start_date)
         print(end_date)
 
+
         daterange_sql = f"SELECT SUM(Tr.commission_paid) , COUNT(*) FROM Transaction Tr"
 
         try:
